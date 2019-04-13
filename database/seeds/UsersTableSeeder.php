237a85d7567ae87401ed7f334bb590433a2ada10
hevalnbutala <hb248@njit.edu>
2019-04-13 10:28:53 -0400

    5.  Create a factory for cars that sets the make field to either ford / honda / toyota

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,7 +11,9 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
 
-    {factory(App\User::class, 50)->create()->each(function ($user) {
-        //$user->posts()->save(factory(App\Post::class)->make());
-    });
+    {
+        factory(App\User::class, 50)->create()->each(function ($user) {
+            //$user->posts()->save(factory(App\Post::class)->make());
+        });
+    }
     }
